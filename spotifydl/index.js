e36853204166e8238e2eadcd4e9b93b02e01cd7a
Omar Hussein <omarelwy@hotmail.com>
2021-01-29 13:38:48 +0200

    🔊 Added new log on starting downloading

--- spotifydl/index.js
+++ spotifydl/index.js
@@ -7,7 +7,8 @@ function spotifydl() {
   startDialog({ question: `Enter Spotify link you want to download.` })
   process.stdin.on("data", data => {
     const answer = data.toString().trim()
-
+    if (!answer.match(/open.spotify.com/)) return
+    console.log("\n  Downloading...")
     exec(`spotifydl ${answer} -o ${musicRootFolder}`, (error, stdout, stderr) => {
       if (error) return console.log("error", error)
 
