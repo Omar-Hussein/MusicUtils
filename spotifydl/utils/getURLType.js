39420fe1d3c361a88ca52c3ddc8a2acbe614f534
Omar Hussein <omarelwy@hotmail.com>
2021-01-31 11:52:42 +0200

    🔇 Remove throwing error on not not getting the URL type

--- spotifydl/utils/getURLType.js
+++ spotifydl/utils/getURLType.js
@@ -3,8 +3,6 @@ function getURLType(url) {
   if (url.match(/\/album\//)) return "Album"
   if (url.match(/\/playlist\//)) return "Playlist"
   if (url.match(/\/artist\//)) return "Artist"
-
-  throw new Error(`Invalid spotify URL`)
 }
 
 module.exports = getURLType
