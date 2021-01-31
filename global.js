fd382c80d04cf580d8ee2ddd10ea80013399a649
Omar Hussein <omarelwy@hotmail.com>
2021-01-31 05:42:36 +0200

    ♻️ Refactor the global file (remove path util)

--- global.js
+++ global.js
@@ -1,12 +1,9 @@
-const path = require("path")
-
 module.exports = {
   globalSourceFolderName: "src_files",
-  musicRootFolder: path.resolve(`${__dirname}`, `.\\${this.globalSourceFolderName}\\run`),
-  musicToSetTagsFolder: path.resolve(`${__dirname}`, `.\\${this.globalSourceFolderName}\\set_tags`),
-  backupFolder: path.resolve(`${__dirname}`, `.\\${this.globalSourceFolderName}\\backup`),
+  musicRootFolder: `${__dirname}\\src_files\\run`,
+  musicToSetTagsFolder: `${__dirname}\\src_files\\set_tags`,
+  backupFolder: `${__dirname}\\src_files\\backup`,
 
-  notFileNameFormat: /(\\|\||\?|\*|<|>|:|\/|"|\.$)/g,
   audioFilesFormat: /(ac3|m4a|au|snd|pcm|mid|rmi|mp3|mp4|aif|aifc|aiff|m3u|ra|ram|ogg|vorbis|wav|flac)$/i,
   imagesFilesFormat: /(gif|jpe?g|tiff|png)$/i,
 }
