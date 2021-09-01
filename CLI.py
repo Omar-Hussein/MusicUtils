90a5edddfeeae58b7e299dba80de15160ee3e8b4
Omar Hussein <omarelwy@hotmail.com>
2021-09-01 10:27:25 +0200

    ♻️ Update CLI

--- CLI.py
+++ CLI.py
@@ -1,5 +1,9 @@
-from utils import text, choose, remove_empty_folders
+import re
+import argparse
+
+from utils import remove_empty_folders
 from config import RUN_FOLDER
+from exceptions import InvalidLink
 
 from rearrange import rearrange
 from download import download
@@ -7,38 +11,51 @@ from move_to_lib import move
 from lyrics import get_lyrics
 
 
-APP_OPTIONS = [
-    "Download via Deezer or Spotify link",
-    "Rearrange music files",
-    "Move files to music folder",
-    "Remove empty folders"
-    "Get lyrics"
-]
-
-
 def main():
-    index = choose("What to do?", APP_OPTIONS)
+    actions = ["download", "move", "arrange", "lyrics", "remove-empty"]
+    parser = argparse.ArgumentParser(description="Music utils app")
 
-    if index == 0:
-        link = text("Enter link")
-        download(link, verbose=True)
+    parser.add_argument("action", choices=actions,
+                        help="action you'd like to preform")
+    action = parser.parse_args().action
 
-    if index == 1:
-        rearrange(verbose=True)
+    parser.add_argument(
+        "--link", help="link to download (with download action)", required=action == "download")
 
-    if index == 2:
+    parser.add_argument(
+        "--song", help="link to download (with lyrics action)", required=action == "lyrics")
+    parser.add_argument(
+        "--artist", help="link to download (with lyrics action)")
+
+    parser.add_argument(
+        "--verbose", "-v", action=argparse.BooleanOptionalAction, default=True,
+        help="make the action verbose")
+
+    args = parser.parse_args()
+    verbose = args.verbose
+
+    if action == "move":
         move()
 
-    if index == 3:
+    elif action == "arrange":
+        rearrange(verbose=verbose)
+
+    elif action == "lyrics":
+        song = args.song
+        artist = args.artist
+
+        lyrics = get_lyrics(song, artist, verbose=verbose)
+        print(lyrics)
+
+    elif action == "download":
+        link = args.link
+
+        if not re.match(r"https://open.spotify.com/(album|playlist|artist)/*", link):
+            raise InvalidLink(link)
+        download(link, verbose=verbose)
+
+    elif action == "remove-empty":
         remove_empty_folders(RUN_FOLDER)
-
-    if index == 4:
-        artist = text("Enter artist name")
-        song = text("Enter song name")
-        get_lyrics(song, artist)
-
-    print("")
-    main()
 
 
 if __name__ == "__main__":
