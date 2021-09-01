7efe1c592f367fe5ea722cf9cace066fd5153afb
Omar Hussein <omarelwy@hotmail.com>
2021-09-01 07:19:25 +0200

    🔨 Update the script running flask

--- scripts/start-api.ps1
+++ scripts/start-api.ps1
@@ -1,7 +1,11 @@
 $app_location = "E:\WebDev\MusicUtils\"
 
 & ($app_location + "env\Scripts\Activate.ps1")
+
 $env:FLASK_APP = $app_location + "API.py"
-flask run --port 5001
+$env:FLASK_ENV = "development"
+$env:FLASK_RUN_PORT = 5001
+
+flask run
 
 pause
